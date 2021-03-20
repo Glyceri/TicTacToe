970a0affebab9813e976409982e6e1ac593d56dd
Glyceri <78602618+Glyceri@users.noreply.github.com>
2021-03-20 15:28:01 +0100

    New Update

---
+++ TicTacToe/Game.py
@@ -0,0 +1,5 @@
+class Game:
+    Board = None
+    boardSize = 0
+    name1 = str
+    name2 = str
--- TicTacToe/app.py
+++ TicTacToe/app.py
@@ -1,15 +1,60 @@
-import extra
+from Game import Game
 Quit = False
-Board = [[" ", " ", " "],[" ", " ", " "],[" ", " ", " "]]
+
+def QuitCheck():
+    print("Want to play another round. Type 'y' or 'n'")
+    ShouldQuit = str(input())
+    if ShouldQuit == 'n':
+        return True
+    elif ShouldQuit == 'y':
+        return False
+    else:
+        print("Please input a 'y' or 'n'")
+        return QuitCheck()
+
+def NameCheck():
+    print("Player One please enter your name:")
+    gameRules.name1 = str(input())
+    print("Welcome:", gameRules.name1)
+    print("Player Two please enter your name:")
+    gameRules.name2 = str(input())
+    print("Welcome:", gameRules.name2)
+    return
+
+def SetupBoard(size):
+    gameRules.boardSize = size
+    gameRules.Board = [" "] * ((size + 1) * (size + 1))
+    SetNode(0, "X")
+    return
+
+def PrintBoard():
+    counter = 0
+    board_text = ""
+    for i in range(len(gameRules.Board)):
+
+        counter += 1
+        board_text += "[" + gameRules.Board[i] + "]"
+
+        if counter > gameRules.boardSize:
+            counter = 0
+            print(board_text)
+            board_text = ""
+
+def SetNode(selector, setToo):
+    gameRules.Board[selector] = setToo
+        
 
 
 while Quit == False:
-    Board = [[" ", " ", " "],[" ", " ", " "],[" ", " ", " "]]
     print("Welcome to Tic Tac Toe")
-    extra.PrintBoard()
-    extra.NameCheck()
+    gameRules = Game()
+    SetupBoard(3)
+    PrintBoard()
+    NameCheck()
     
-    Quit = extra.QuitCheck()
+
+
+    Quit = QuitCheck()
 
 
 
